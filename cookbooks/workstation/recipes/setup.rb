f5a5e5f7f1340fc1c35d5d4ec6ecf6b560e8b6a1
nagaharish12 <naga.yerrarapu@dc.gov>
2019-06-02 23:50:13 +0000

    changes to workstation recipe

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -13,17 +13,29 @@ end
 package 'ntp' 
 
 
-template '/etc/motd' do
-  source 'motd.erb'
-  variables(
-    :name => 'nagharish'
-  )
-  action :create
-end
+#template '/etc/motd' do
+#  source 'motd.erb'
+#  variables(
+#    :name => 'nagharish'
+#  )
+#  action :create
+#end
 
 service 'ntpd' do
   action [ :enable, :start ]
 end
+
+user 'user1' do
+  comment 'user1'
+  uid '123'
+  home '/home/user1'
+  shell '/bin/bash'
+end
+
+group 'admins' do
+  members ['user1']
+end
+
 
 
 
