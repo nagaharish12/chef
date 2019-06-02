7fde0daceee12fd5af41eb906274be5d624bc827
nagaharish12 <naga.yerrarapu@dc.gov>
2019-06-02 23:30:40 +0000

    changes in server.rb

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: apache
+cookbook Name:: apache
 # Recipe:: server
 #
 # Copyright (c) 2019 The Authors, All Rights Reserved.
@@ -10,14 +10,47 @@ package 'httpd' do
   action :install
 end
 
-cookbook_file '/var/www/html/index.html' do
-  source 'index.html'
+bash "inline script"  do
+  user "root"
+  code "mkdir -p /var/www/mysites/ && chown -R apache /var/www/mysites/"
+#  not_if '[ -d /var/www/mysites/ ]'
+  not_if do
+    File.directory?('/var/www/mysites/')
+  end
 end
 
-#template '/var/www/html/index.html' do
-#  source 'index.html.erb'
-#  action :create
+execute "run a script" do
+  user "root"
+  command <<-EOH
+  mkdir -p /var/www/mysites/ /
+  chown -R apache /var/www/mysites/
+  EOH
+  not_if
+end
+
+#execute "run script" do
+#  user "root"
+#  command './myscript.sh'
+#  not_if
 #end
+
+directory "/var/www/mysites/" do
+  owner "apache"
+  recursive true
+end
+
+#cookbook_file '/var/www/html/index.html' do
+#  source 'index.html'
+#end
+
+#remote_file '/var/www/html/harish.png' do
+#  source 'https://www.google.com/search?hl=en&tbm=isch&source=hp&biw=1280&bih=605&ei=5zX0XJrkFc29gge0v6GgBw&q=aws&oq=aws&gs_l=img.3..0l10.3270.3726..3925...0.0..0.162.259.2j1......0....1..gws-wiz-img.....0.jpir72Wy9oE#imgrc=Hbx6kYxtn5F10M:'
+#end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create
+end
 
 service 'httpd' do
   action [ :enable, :start ]
