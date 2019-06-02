07ec76dcda9623b8f9afbebdb9f9dfac3e1f0f66
nagaharish12 <naga.yerrarapu@dc.gov>
2019-06-02 03:21:43 +0000

    changes in apache recipe

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -10,10 +10,14 @@ package 'httpd' do
   action :install
 end
 
-template '/var/www/html/index.html' do
-  source 'index.html.erb'
-  action :create
+cookbook_file '/var/www/html/index.html' do
+  source 'index.html'
 end
+
+#template '/var/www/html/index.html' do
+#  source 'index.html.erb'
+#  action :create
+#end
 
 service 'httpd' do
   action [ :enable, :start ]
